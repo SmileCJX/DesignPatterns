da012555925a2a70c61678f185ed1b1225d09d7e
SmileCJX <1092925421@qq.com>
2017-09-21 00:51:09 +0800

    运算操作类及其继承类

---
+++ src/pers/caijx/designspatterns/factory/v02/OperationSub.java
@@ -0,0 +1,15 @@
+package pers.caijx.designspatterns.factory.v02;
+
+/**
+ * 减法类，继承运算类
+ * Created by Administrator on 2017/8/6/006.
+ */
+public class OperationSub extends Operation {
+
+    @Override
+    public double getResult() {
+        double result = 0;
+        result = getNumberA() - getNumberB();
+        return result;
+    }
+}
